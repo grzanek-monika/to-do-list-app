ad2700bed4c1e72380d92d3c15b568a821cdf6d4
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-15 08:07:17 +0200

    Add Container component, add icons to columns and changes in buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Container from './components/Container/Container.js';
 import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SearchForm.js';
+import SearchForm from './components/SearchForm/SerachForm.js';
 import List from './components/List/List.js';
 
 const App = () => {
   return (
     <div>
-      <Hero />
-      <SearchForm />
-      <List />
+      <Container>
+        <Hero />
+        <SearchForm />
+        <List />
+      </Container>  
     </div> 
   );
 };
