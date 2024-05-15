42682c9b451e71956a709ae4477f7b3fe33c211c
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-15 08:09:36 +0200

    Correct in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from './components/Container/Container.js';
 import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SerachForm.js';
+import SearchForm from './components/SearchForm/SearchForm.js';
 import List from './components/List/List.js';
 
 const App = () => {
