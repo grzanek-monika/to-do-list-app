e62d098dc78bc778d3803040ce52cfac5885569a
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-21 07:27:45 +0200

    Add initialState, move data to initialState and import initialState into storage.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className={styles.hero}>
           <h1 className={styles.title}>My first React App</h1>
-            <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
+            <p className={styles.subtitle}>A simple to-do app with lists, columns and card</p>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store'
@@ -7,11 +7,11 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.querySelector('#root')
+  </React.StrictMode>
 );
