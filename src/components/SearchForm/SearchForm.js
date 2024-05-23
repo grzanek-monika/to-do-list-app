2db08ff57b65fba56ee2ea5428b896224dc74461
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-23 14:08:28 +0200

    Add new property in initialState, add new action in reducer, add useState, useDispatch and searchButton function in SearchForm component, change in cards in Column component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,22 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+  const dispatch = useDispatch();
+  const [txt, setTxt] = useState('');
+  const searchButton = e => {
+    e.preventDefault();
+    dispatch({
+      type: "UPDATE_SEARCHSTRING",
+      payload: txt
+    })
+  }
     return(
-      <form className={styles.searchForm}>
-        <TextInput placeholder='Search...' />
+      <form className={styles.searchForm} onSubmit={searchButton}>
+        <TextInput placeholder='Search...' value={txt} onChange={e => setTxt(e.target.value)}/>
         <Button>
           <span className="fa fa-search" />
         </Button>
