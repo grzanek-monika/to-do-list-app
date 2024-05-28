5e324010b84d92fd620d9fdd166b8e820b7569d0
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-28 14:04:47 +0200

    Add actions ADD_LIST and TOGGLE_CARD_FAVORITE in reducer, add addList and toggleCardFavorite creators, add ListForm component and call it in Lists component, add new property isFavorite to cards in initialState.

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,28 @@
+import styles from './ListForm.module.scss';
+import Button from "../Button/Button.js";
+import TextInput from "../TextInput/TextInput.js";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+
+const ListForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({title, description}));
+    }
+
+    return(
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <span className={styles.inputStyle}>Title: </span>
+            <TextInput value={title} placeholder='Title' onChange={ e => setTitle(e.target.value)}  />
+            <span className={styles.inputStyle}> Description: </span>
+            <TextInput value={description} placeholder='Description' onChange={ e => setDescription(e.target.value)}  />
+            <Button>Add list</Button>
+        </form>
+    )
+}
+
+export default ListForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,8 @@ export const getFilteredCards = ({cards, searchString}, columnId) => cards
 
 export const getAllColumns = (state => state.columns);
 
+export const getAllCards = (state => state.cards);
+
 export const getListById = ({lists}, listId) => lists.find(list => list.id === listId);
 
 export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
@@ -25,7 +27,13 @@ const reducer = (state, action) => {
     case 'ADD_CARD':
       return { ...state, cards: [...state.cards, {id: shortid(),  ...action.payload }]};
     case 'UPDATE_SEARCHSTRING':
-      return { ...state, searchString: action.payload }
+      return { ...state, searchString: action.payload };
+    case 'ADD_LIST': 
+      return {...state, lists: [...state.lists, {id: shortid(), ...action.payload }]};
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? 
+        { ...card, isFavorite: !card.isFavorite } : card) };
+  
     default: 
       return state;
   }
@@ -34,7 +42,9 @@ const reducer = (state, action) => {
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
-export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payload })
+export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payload });
+export const addList = payload => ({type: 'ADD_LIST', payload});
+export const toggleCardFavorite = payload => ({type: 'TOGGLE_CARD_FAVORITE', payload });
 
 
 const store = createStore(
