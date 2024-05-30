010ba97ac3126072b502d4bd4da47f9c784a3d11
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-30 14:08:58 +0200

    Add subreducers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers  } from 'redux';
 import initialState from './initialState.js';
 import shortid from 'shortid';
 import { strContains } from '../utils/strContains.js';
@@ -20,25 +20,58 @@ export const getAllLists = (state => state.lists);
 
 export const getSearchString = ({ searchString }) => searchString;
 
+// subreducers
+const listsReducer = (statePart = [], action) => {
+  switch(action.type) {
+    case 'ADD_LIST':
+      return [...statePart, { ...action.payload, id: shortid() }];
+    default:
+      return statePart;
+  }
+}
+
+const columnsReducer = (statePart = [], action) => {
+  switch(action.type) {
+    case 'ADD_COLUMN':
+      return [...statePart, { ...action.payload, id: shortid() }];
+    default:
+      return statePart;
+  }
+}
+
+const cardsReducer = (statePart = [], action) => {
+  switch(action.type) {
+    case 'ADD_CARD':
+      return [...statePart, { ...action.payload, id: shortid() }];
+    case 'TOGGLE_CARD_FAVORITE':
+      return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+    default:
+      return statePart;
+  }
+}
+
+const searchStringReducer = (statePart = '', action) => {
+  switch(action.type) {
+    case 'UPDATE_SEARCHSTRING':
+      return action.payload
+    default:
+      return statePart;
+  }
+}
+
+
 
 const reducer = (state, action) => {
-  switch(action.type){
-    case 'ADD_COLUMN' : 
-      return { ...state, columns: [...state.columns, {id: shortid(), ...action.payload}]};
-    case 'ADD_CARD':
-      return { ...state, cards: [...state.cards, {id: shortid(),  ...action.payload }]};
-    case 'UPDATE_SEARCHSTRING':
-      return { ...state, searchString: action.payload };
-    case 'ADD_LIST': 
-      return {...state, lists: [...state.lists, {id: shortid(), ...action.payload }]};
-    case 'TOGGLE_CARD_FAVORITE':
-      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? 
-        { ...card, isFavorite: !card.isFavorite } : card) };
-  
-    default: 
-      return state;
-  }
+  const newState = {
+    lists: listsReducer(state.lists, action),
+    columns: columnsReducer(state.columns, action),
+    cards: cardsReducer(state.cards, action),
+    searchString: searchStringReducer(state.searchString, action)
+  };
+
+  return newState;
 };
+
 
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
